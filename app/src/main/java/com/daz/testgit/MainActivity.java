42e6cb14653f522a2c7c0bb56964744e34deb338
dazheng <ydz_1130@163.com>
2016-03-16 11:06:51 +0800

    commit

--- app/src/main/java/com/daz/testgit/MainActivity.java
+++ app/src/main/java/com/daz/testgit/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         Log.d("dazheng", "test git");
-        // TODO test
+        // TODO test  only commit
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
